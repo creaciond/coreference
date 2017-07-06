4c7bd22372e74709bc3e907ac3e4538b9ef93ea5
Daria Maximova <daria.maximova.m@gmail.com>
2017-07-06 13:14:36 +0300

    finish adding morphological markup
    морфологическая разметка вроде всё

--- add_morphology.py
+++ add_morphology.py
@@ -1,27 +1,42 @@
+from pymorphy2 import tokenizers
 from pymorphy2 import MorphAnalyzer
 import os
 
 
-def split_into_tokens(text):
-    tokens = []
-    '''
-        tokens
-        -> sentence1: [token1:(features1), token2:(features2), ...]
-    '''
-    return tokens
+def morphology_features(tokens, morph):
+    new_tokens = []
+    for token in tokens:
+        analysis = str(morph.parse(token)[0].tag)
+        new_tokens.append(token + '\t' + analysis)
+    return new_tokens
+
+
+def write_info(tokens, path, fname):
+    for i in range(len(tokens)):
+        tokens[i] = str(i+1) + '\t' + tokens[i]
+    path = path.replace('rucoref_texts', 'rucoref_new_parsed')
+    if not os.path.exists(path):
+        os.makedirs(path)
+    fpath = path + os.sep + fname
+    with open(fpath, 'w', encoding='utf-8') as new_file:
+        new_file.write('\n'.join(tokens))
 
 
 def main():
     morph = MorphAnalyzer()
-    texts_path = '..\\RuCoref\\rucoref_texts'
+    texts_path = '..' + os.sep + '..' + os.sep + 'RuCoref' + os.sep + 'rucoref_texts'
     for folder in os.listdir(texts_path):
-        text_folder = texts_path + '\\' + folder
-        for file in os.listdir(text_folder):
-            if file.endswith('.txt'):
-                file = text_folder + '\\' + file
-                print(file)
-                with open(file, 'r', encoding='utf-8') as source:
-                    tokens = split_into_tokens(source.read())
+        text_folder = texts_path + os.sep + folder
+        for filename in os.listdir(text_folder):
+            if filename.endswith('.txt'):
+                with open(text_folder + os.sep + filename, 'r', encoding='utf-8') as source_file:
+                    source_text = source_file.read()
+                    # get tokens
+                    tokens = tokenizers.simple_word_tokenize(source_text)
+                    # parse tokens
+                    tokens_with_tags = morphology_features(tokens, morph)
+                    # write tokens to new file
+                    write_info(tokens_with_tags, text_folder, filename)
 
 
 if __name__ == '__main__':
