a3deeadb6ad884f33e62ab2af5bf241365af53f8
Daria Maximova <daria.maximova.m@gmail.com>
2017-07-11 13:50:24 +0300

    fix <tag>, add <source>
    Теперь собираются единые предложения

--- markup_to_opencorpora_xml.py
+++ markup_to_opencorpora_xml.py
@@ -8,7 +8,7 @@ def head(text_params):
     head_lines.append('<text name=\"%s\" id=\"%d\" parent=\"%d\">' % (text_params['name'], text_params['id'], text_params['parent']))
     head_lines.append('<tags>')
     head_lines.append('  <tag>Год: %s</tag>' % text_params['year'])
-    head_lines.append('  <tag>Дата:  %s</tag>' % text_params['date'])
+    head_lines.append('  <tag>Дата: %s</tag>' % text_params['date'])
     head_lines.append('  <tag>Автор: %s</tag>' % text_params['author'])
     if isinstance(text_params['topic'], list):
         topics = ['  <tag>Тема: %s</tag>' % item for item in text_params['topic']]
@@ -19,17 +19,41 @@ def head(text_params):
     return '\n'.join(head_lines)
 
 
+def main_xml(text):
+    lines = []
+    # blank line (\n\n) separates two paragraphs
+    paragraphs = text.split('\n\n')
+    for paragraph in paragraphs[1:]:
+        sentence = []
+        tokens_xml = []
+        # each line — new analysis
+        tokens = paragraph.split('\n')
+        for token in tokens:
+            # features are separated by \t
+            features = token.split('\t')
+            # 2nd feature: Text=wordform
+            sentence.append(features[1].split('=')[1])
+        lines.append('<source>' + ' '. join(sentence) + '</source>')
+    return lines
+
+
+
 def main():
     text_params = {
-        'name': 'name',
+        'name': 'document_name',
         'id': 0,
         'parent': 0,
         'year': '0000',
         'date': '00/00',
         'author': 'some author',
-        'topic': ['item1', 'item1/morespecific1']
+        'topic': 'item1'
     }
-    print(head(text_params))
+    # head_document = head(text_params)
+    with open('..' + os.sep + '140490813-#dev_conll_1_morph.csv', 'r', encoding='utf-8') as f:
+        lines = f.read()
+    paragraph = main_xml(lines)
+    for line in paragraph:
+        print(line)
 
 
 if __name__ == '__main__':
